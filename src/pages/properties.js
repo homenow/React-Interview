60dac96fbce9e63ab7a652f3f2987f971ff151e2
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-12-15 09:39:25 -0600

    Finished navbar

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
 import NavBar from '../components/navBar';
+import TableProperty from '../components/tableProperty';
 
 export default function Properties() {
   return (
     <div>
       <NavBar />
+      <div>
+        <h1>My Properties</h1>
+        <Button variant="contained" color="primary">
+          Add Property
+        </Button>
+        <TableProperty />
+      </div>
     </div>
   );
 }
