aba2f6ceddd53778d40d6199893f534afcabeea8
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-12-15 09:30:51 -0600

    Added navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles(() => ({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyle();
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>hello world</Toolbar>
+    </AppBar>
+  );
+}
--- src/pages/properties.js
+++ src/pages/properties.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import NavBar from '../components/navBar';
 
 export default function Properties() {
   return (
