a451f9114f0f858febc661f38fa9e3a3b282c6fe
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-12-15 09:47:07 -0600

    Finished formatin propeties.js page

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 import NavBar from '../components/navBar';
 import TableProperty from '../components/tableProperty';
 
+const useStyles = makeStyles(() => ({
+  headline: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginLeft: '5vw',
+  },
+  addButton: {
+    marginLeft: '5vw',
+  },
+  primaryContainer: {
+    marginLeft: '20vw',
+    marginRight: '20vw',
+  },
+}));
 export default function Properties() {
+  const classes = useStyles();
+
   return (
     <div>
       <NavBar />
-      <div>
-        <h1>My Properties</h1>
-        <Button variant="contained" color="primary">
-          Add Property
-        </Button>
+      <div className={classes.primaryContainer}>
+        <div className={classes.headline}>
+          <h1>My Properties</h1>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.addButton}
+          >
+            Add Property
+          </Button>
+        </div>
         <TableProperty />
       </div>
     </div>
