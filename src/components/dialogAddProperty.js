58260c4c6ee36feb8821fb9f4e90a8d0f5d1520b
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-12-15 10:22:58 -0600

    Adding form to modal/dialog

--- src/components/dialogAddProperty.js
+++ src/components/dialogAddProperty.js
@@ -1,6 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Dialog, Box } from '@material-ui/core/';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  MenuItem,
+  Select,
+  Box,
+  Typography,
+  TextField,
+} from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
@@ -12,14 +21,59 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export default function DialogAddPropertyHouseInfo({ open }) {
+export default function DialogAddPropertyHouseInfo({ open, onClose }) {
   const classes = useStyles();
+  const handleClose = () => {
+    onClose(false);
+  };
+  const [data, setData] = React.useState({
+    type: '',
+    floors: 0,
+  });
 
+  const handleChange = (event) => {
+    setData((oldValues) => ({
+      ...oldValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
   return (
-    <Dialog className={classes.dialog} open={open}>
-      <Box>
-        <div>hello world</div>
-      </Box>
-    </Dialog>
+    <div>
+      <Dialog
+        className={classes.dialog}
+        open={open}
+        onClose={handleClose}
+        fullWidth="true"
+        maxWidth="sm"
+      >
+        <Box component="form">
+          <DialogTitle>Add Property</DialogTitle>
+          <DialogContent className={classes.pageContainer}>
+            <Typography>Property Type</Typography>
+
+            <Select
+              value={data.type}
+              displayEmpty
+              onChange={handleChange}
+              inputProps={{
+                name: 'type',
+                id: 'type',
+              }}
+            >
+              <MenuItem value="singleFamilyHouse">Single Family House</MenuItem>
+              <MenuItem value="condominium">Condominium</MenuItem>
+              <MenuItem value="apartment">Apartment</MenuItem>
+            </Select>
+
+            <Typography>Property Nickname</Typography>
+            <TextField
+              value={data.nickname}
+              name="nickname"
+              onChange={handleChange}
+            />
+          </DialogContent>
+        </Box>
+      </Dialog>
+    </div>
   );
 }
