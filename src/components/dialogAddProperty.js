f5d6898d190f906d43418cfae6207e9973bf26c1
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-12-15 10:36:34 -0600

    Added second dialog to mimic carousel ui, will start adding form fields next.

--- src/components/dialogAddProperty.js
+++ src/components/dialogAddProperty.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+//  import { useForm, Controller } from 'react-hook-form';
 import {
   Dialog,
   DialogTitle,
@@ -9,20 +10,47 @@ import {
   Box,
   Typography,
   TextField,
+  Button,
+  CircularProgress,
+  fade,
 } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
+
+//  import DialogAddPropertyHouseInfo from './dialogAddPropertyHouseInfo';
 
 const useStyles = makeStyles(() => ({
   dialog: {
     root: {
-      width: '50vw',
-      height: '80vh',
+      width: '80vw',
+
+      position: 'relative',
     },
+    paper: {
+      minHeight: '80vh',
+      maxHeight: '80vh',
+    },
+  },
+  pagecontainer: {
+    display: 'grid',
+  },
+  backdrop: {
+    position: 'absolute',
+    backgroundColor: fade('#D3D3D3', 0.5),
+    zIndex: 0,
+    width: '100%',
+    height: '100%',
+  },
+  spinner: {
+    position: 'relative',
+    left: '50%',
+    top: '50%',
   },
 }));
 
-export default function DialogAddPropertyHouseInfo({ open, onClose }) {
+export default function DialogAddProperty({ open, onClose }) {
   const classes = useStyles();
+  const [secondPageOpen, setSecondPageOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const handleClose = () => {
     onClose(false);
   };
@@ -37,6 +65,21 @@ export default function DialogAddPropertyHouseInfo({ open, onClose }) {
       [event.target.name]: event.target.value,
     }));
   };
+
+  const submitForm = async () => {
+    const promise = new Promise((res) => {
+      console.log('start spinner');
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        return res('done');
+      }, 2000);
+    });
+
+    const result = await promise;
+    console.log('the data :', data, result);
+  };
+
   return (
     <div>
       <Dialog
@@ -71,8 +114,37 @@ export default function DialogAddPropertyHouseInfo({ open, onClose }) {
               name="nickname"
               onChange={handleChange}
             />
+            <Button
+              onClick={() => {
+                //  setFirstPageOpen(false);
+                onClose(false);
+                setSecondPageOpen(true);
+                //  console.log('open , firstpageOpen', open, firstPageOpen);
+              }}
+            >
+              Next
+            </Button>
           </DialogContent>
         </Box>
+      </Dialog>
+      <Dialog className={classes.dialog} open={secondPageOpen}>
+        <Box component="form">
+          <DialogTitle>Add Property Details</DialogTitle>
+          <DialogContent>
+            <Typography>Property Nickname</Typography>
+            <TextField
+              name="floors"
+              value={data.floors}
+              onChange={handleChange}
+            />
+            <Button onClick={submitForm}>Save and Close</Button>
+          </DialogContent>
+        </Box>
+        {loading && (
+          <div className={classes.backdrop}>
+            <CircularProgress color="inherit" className={classes.spinner} />
+          </div>
+        )}
       </Dialog>
     </div>
   );
